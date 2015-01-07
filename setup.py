6386cb5dfef34561fc1de3252d51f47fb98f9c58
Christopher AMIAUD <christopher.amiaud@gmail.com>
2015-01-07 10:30:32 +0100

    minor fixe

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class PyTest(Command):
 
 setup(
     name='pytaskpool',
-    version='1.0',
+    version='1.1',
 
     description='A simple multiprocessing function pool',
     long_description=long_description,
@@ -41,7 +41,7 @@ setup(
     author_email='christopher.amiaud@gmail.com',
 
     url='https://github.com/govlog/pytaskpool',
-    download_url='https://github.com/govlog/pytaskpool/tarball/1.0',
+    download_url='https://github.com/govlog/pytaskpool/tarball/1.1',
 
     license='GPL',
 
